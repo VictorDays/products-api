46f101ecaa3299c44eb4a3dd2ac3063fa4c289b3
VictorAlvesD <victoralvesrdias@gmail.com>
2024-01-22 20:05:52 -0300

    Criação do Repository com JpaRepository

---
+++ src/main/java/com/spring/apirest/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.spring.apirest.repositories;
+
+import com.spring.apirest.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+
+}
