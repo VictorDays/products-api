baa873a5456cc5762432b39bbd5e3b788b05bd29
VictorAlvesD <victoralvesrdias@gmail.com>
2024-02-27 17:00:23 -0300

    Implementando o securityfilterchain

---
+++ src/main/java/com/spring/apirest/config/security/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.spring.apirest.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.HttpSecurityDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+    //Corrente de filtros de segurança
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) //criando uma plitica de sessão em que não guardamos o estado
+                .build();
+    }
+}
