48860d1b815d6c538def9f75b41818058ca7ffc4
VictorAlvesD <victoralvesrdias@gmail.com>
2024-02-27 16:23:56 -0300

    Criando os atributos de user e implementando interface UserDetails

--- src/main/java/com/spring/apirest/models/users/User.java
+++ src/main/java/com/spring/apirest/models/users/User.java
@@ -1,4 +1,95 @@
 package com.spring.apirest.models.users;
 
-public class User {
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_USER")
+public class User implements UserDetails {
+    //UserDetails é uma interface fornecida pelo Spring Security que representa os detalhes de um usuário
+    //do sistema. Esta interface define métodos para obter informações sobre o usuário,
+    //como nome de usuário, senha, autorizações (ou papéis) e se a conta do usuário está habilitada.
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idUser;
+
+    private String login;
+
+    private String passaword;
+
+    private String role;
+
+    public UUID getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(UUID idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassaword() {
+        return passaword;
+    }
+
+    public void setPassaword(String passaword) {
+        this.passaword = passaword;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    //Metodos UserDetails:
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.passaword;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
