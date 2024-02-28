96ec224ad34b5662e4512dfaae769bab004d7685
VictorAlvesD <victoralvesrdias@gmail.com>
2024-02-28 09:52:14 -0300

    Criando serviço para gerar Token JWT

---
+++ src/main/java/com/spring/apirest/config/security/TokenService.java
@@ -0,0 +1,39 @@
+package com.spring.apirest.config.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.spring.apirest.models.users.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    //Essa anotação é usada para injetar o valor da propriedade api.security.token.secret definida no arquivo de propriedades.
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token =  JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(generationExpirationDate())
+                    .sign(algorithm);
+            return token;
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Error while generation token", exception);
+        }
+    }
+
+    // retorna uma instância de Instant que representa a data de expiração de um token JWT.
+    private Instant generationExpirationDate(){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
