c5d37f4ea082c74748e289c3f8c22ba391e5878b
VictorAlvesD <victoralvesrdias@gmail.com>
2024-02-28 09:57:10 -0300

    Criando serviço para validar Token JWT

--- src/main/java/com/spring/apirest/config/security/TokenService.java
+++ src/main/java/com/spring/apirest/config/security/TokenService.java
@@ -3,6 +3,7 @@ package com.spring.apirest.config.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.spring.apirest.models.users.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,20 @@ public class TokenService {
         }catch (JWTCreationException exception){
             throw new RuntimeException("Error while generation token", exception);
         }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException exception){
+            return " ";
+        }
+
     }
 
     // retorna uma instância de Instant que representa a data de expiração de um token JWT.
